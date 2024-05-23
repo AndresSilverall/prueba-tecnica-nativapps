023bbbdc807a1a15c56d121b07c6a04ff77767c5
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-05-23 14:38:48 -0500

    Manejo de excepciones agregadas a las funciones de llamada a la API

--- frontend/booking-flight/src/api/getBookedFlights.js
+++ frontend/booking-flight/src/api/getBookedFlights.js
@@ -1,7 +1,15 @@
 // Llamada a la API para devolver los vuelos reservados
 export const fetchBookedFLights = async () => {
-	const URL = "http://127.0.0.1:8000/api/show_bookings";
-	let response = await fetch(URL);
-	let bookedFlights = await response.json();
-	return bookedFlights; 
-}
+    try {
+				const URL = "http://127.0.0.1:8000/api/show_bookings";
+        let response = await fetch(URL);
+				if (!response.ok) {
+					throw new Error({"error": response.status})
+				}
+        let bookedFlights = await response.json();
+        return bookedFlights; 
+
+    } catch (error) {
+        console.log({"error": error.message})
+    }
+};
--- frontend/booking-flight/src/api/getFlights.js
+++ frontend/booking-flight/src/api/getFlights.js
@@ -1,9 +1,17 @@
 // Llamada a la API para obtener todos los vuelos disponibles
 export const fetchFlights = async () => {
-    // URL con los endpoints de la API
-    const URL = "http://127.0.0.1:8000/api/flights";
-    let response = await fetch(URL);
-    let flight = await response.json()
-    return flight;
+    try {
+        // URL con los endpoints de la API
+        const URL = "http://127.0.0.1:8000/api/flights";
+        let response = await fetch(URL);
 
-    } 
+        if (!response.ok) {
+            throw new Error({"error": response.status})
+        }
+        let flight = await response.json()
+        return flight;
+        
+    } catch (error) {
+        console.log({"error": error.message})
+    }
+};
