fad68b67db7491a4593ba484475851c7d26c66f1
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-05-24 18:11:30 -0500

    Cards agregadas para visualizar detalles de las reservas

--- frontend/booking-flight/src/components/BookedFlights.jsx
+++ frontend/booking-flight/src/components/BookedFlights.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useState, useEffect } from 'react'
 import { fetchBookedFLights } from "../api/getBookedFlights";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane, faToggleOn, faPlane } from '@fortawesome/free-solid-svg-icons';
+import flightBooked from '../images/booked.jpg';
 
 
 
@@ -19,24 +20,50 @@ const GetAllBookedFlights = () => {
 	},[])
 
 	return (
-		<div>
-			<h3 className="text-center lead py-5">Vuelos reservados <FontAwesomeIcon icon={faPaperPlane} /></h3>
-			{booked.map((b) => (
-				<ul key={b.id}>
-					<li>{b.flight}</li>
-					<li>{b.customer}</li>
-					<li>{b.citizenship_card}</li>
-					<li>{b.nationality}</li>
-					<li>{b.phone}</li>
-					<li>{b.passenger}</li>
-					<li>{b.preference}</li>
-					<li>{b.status}</li>
-					<li>{b.created_at}</li>
-				</ul>
+		<div className="container">
+			<h3 className="text-center lead py-5">Lista de vuelos reservados <FontAwesomeIcon icon={faPaperPlane} /></h3>
+			<div className='row'>
+			{booked.map((booking) =>  (
+				<div className='col-md-4 d-flex align-items-stretch'>
+				<div key={booking.id} className="card mb-4 py-4 h-90" style={{ width: '22rem' }}>
+					<div key={booking.id} className="modal fade" id={`bookedFlight-${booking.id}`} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div className="modal-dialog" role="document">
+							<div className="modal-content">
+								<div className="modal-header">
+									<h5 className="modal-title" id="exampleModalLabel">Detalles de la reserva <FontAwesomeIcon icon={faPlane} /></h5>
+									<button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
+										<span aria-bs-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div className="modal-body">
+									<p className="lead">Detalles adicionales del vuelo.</p>
+									<p className="lead">Cliente: {booking.customer} </p>
+									<p className="lead">ID de la reserva: {booking.id} </p>
+									<p className="lead">Asiento asignado: {booking.preference} </p>
+									<p className="lead">Fecha de la reserva:  {booking.created_at}.</p>
+								</div>
+								<div className="modal-footer">
+									<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<img className="img-card" src={flightBooked} alt="booked" />
+					<div className="card-body">
+						<h5 className="card-title text-center"><FontAwesomeIcon icon={faPlane} /> {booking.flight}</h5>
+						<p className="card-text lead text-center"><FontAwesomeIcon icon={faToggleOn} /> Estado: {booking.status}</p>
+						<div className="text-center d-flex btn-group">
+							<button className="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target={`#bookedFlight-${booking.id}`}>Ver más detalles</button>
+							<button className="btn btn-outline-danger" data-bs-toggle="modal" >Cancelar reserva</button>
+					</div>
+					</div>
+				</div>
+				</div>
 			))}
+			</div>
 		</div>
 	);
-}
+};
 
 
 export default GetAllBookedFlights;
